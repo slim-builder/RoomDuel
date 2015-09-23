cfee6e446554769294d967399de9b4f56a7df62f
Steven Lim <stevenlim36@gmail.com>
2015-09-22 23:55:12 -0700

    Add OOP design and introduce magic items

--- duel.js
+++ duel.js
@@ -1,26 +1,106 @@
-var playerHp = 20;
-var opponentHp = 20;
-document.getElementById("player_hp").innerHTML = playerHp;
-document.getElementById("opponent_hp").innerHTML = opponentHp;
+function MagicItem(type) {
+    this.type = type;
+    
+    this.getType = function() {
+        return this.type;
+    }
+    
+    this.use = function(damage) {
+        var actualDamage;
+        switch(this.type)
+        {
+            case 1:
+                actualDamage = 0;
+                break;
+            case 2:
+                actualDamage = -5;
+                break;
+            default:
+                actualDamage = damage;
+        }
+        return actualDamage;
+    }    
+}
+
+function Wizard(type) {
+    this.type = type;
+    this.hp = 20;
+    this.pocket = new Array();
+    
+    this.getHp = function() {
+        return this.hp;
+    };
+
+    this.setHp = function(hp) {
+        this.hp = hp;
+    };
+    
+    this.pickUpItem = function() {
+        this.pocket.push(new MagicItem(Math.floor(2*Math.random()+1)));
+    };
+    
+    this.useItem = function(damage) {
+        var itemObj = this.pocket.pop();
+        if (typeof itemObj != "undefined")
+        {
+            document.getElementById("game_status_info").innerHTML += this.type + " uses an item!";
+            if (itemObj.getType() === 1)
+                document.getElementById("game_status_info").innerHTML += " The item is Negate Attack! " + this.type + " takes no damage! ";
+            else
+                document.getElementById("game_status_info").innerHTML += " The item is Minor Heal! " + this.type + " gains 5 HP! ";
+            return itemObj.use(damage);
+        }
+        else
+        {
+            document.getElementById("game_status_info").innerHTML += this.type + " has no items to use! ";
+            return damage;
+        }
+    };
+    
+    this.emptyPocket = function() {
+        while (this.pocket.length > 0)
+            this.pocket.pop();
+    };
+    
+    this.reset = function() {
+        this.hp = 20;
+        this.emptyPocket();
+    };
+}
+
+var player = new Wizard("Player");
+var opponent = new Wizard("Opponent");
+
+player.pickUpItem();
+opponent.pickUpItem();
+
+document.getElementById("player_hp").innerHTML = player.getHp();
+document.getElementById("opponent_hp").innerHTML = opponent.getHp();
 
 var damageToPlayer;
 var damageToOpponent;
 var gameOver = false;
 
 document.getElementById("reset").onclick=function() {
-    playerHp = 20;
-    opponentHp = 20;
-    document.getElementById("player_hp").innerHTML = playerHp;
-    document.getElementById("opponent_hp").innerHTML = opponentHp;
+    player.reset();
+    opponent.reset();
+    player.pickUpItem();
+    opponent.pickUpItem();
+    document.getElementById("player_hp").innerHTML = player.getHp();
+    document.getElementById("opponent_hp").innerHTML = opponent.getHp();
     gameOver = false;
-    document.getElementById("status_info").innerHTML = "";
+    document.getElementById("game_status_info").innerHTML = "";
     document.getElementById("p_status_info").innerHTML = "";
     document.getElementById("o_status_info").innerHTML = "";
 };
 
-document.getElementById("turn").onclick=function() {
+document.getElementById("turn").onclick = function() {
+    var afterItemDamage;
     if (gameOver === false)
     {
+        document.getElementById("game_status_info").innerHTML = "";
+        document.getElementById("p_status_info").innerHTML = "";
+        document.getElementById("o_status_info").innerHTML = "";
         damageToPlayer = Math.floor(6*Math.random()) + 1;
         damageToOpponent = Math.floor(6*Math.random()) + 1;
         if (!Math.floor(5*Math.random()))
@@ -29,41 +109,54 @@ document.getElementById("turn").onclick=function() {
             document.getElementById("p_status_info").innerHTML = "Absorb electrical power! Player gains " + -damageToPlayer + " HP!";
         }
         else
-            document.getElementById("p_status_info").innerHTML = "Lightning strikes! Player loses " + damageToPlayer + " HP!";
+        {
+            afterItemDamage = player.useItem(damageToPlayer);
+            if (afterItemDamage === damageToPlayer)
+                document.getElementById("p_status_info").innerHTML = "Lightning strikes! Player loses " + damageToPlayer + " HP!";
+            else
+                damageToPlayer = afterItemDamage;
+        }
         if (!Math.floor(5*Math.random()))
         {
-            damageToOpponent = - damageToOpponent;
+            damageToOpponent = -damageToOpponent;
             document.getElementById("o_status_info").innerHTML = "Absorb heat energy! Opponent gains " + -damageToOpponent + " HP!";
         }
         else
-            document.getElementById("o_status_info").innerHTML = "Fireball explodes! Opponent loses " + damageToOpponent + " HP!";
-        if (playerHp - damageToPlayer <= 0 && opponentHp - damageToOpponent <= 0)
         {
-            document.getElementById("status_info").innerHTML = "It's a tie!";
-            playerHp = 0;
-            opponentHp = 0;
+            afterItemDamage = opponent.useItem(damageToOpponent);
+            if (afterItemDamage === damageToOpponent)
+                document.getElementById("o_status_info").innerHTML = "Fireball explodes! Opponent loses " + damageToOpponent + " HP!";
+            else
+                damageToOpponent = afterItemDamage;
+        }
+        
+        if (player.getHp() - damageToPlayer <= 0 && opponent.getHp() - damageToOpponent <= 0)
+        {
+            document.getElementById("game_status_info").innerHTML = "It's a tie!";
+            player.setHp(0);
+            opponent.setHp(0);
             gameOver = true;
         }
-        else if (playerHp - damageToPlayer <= 0)
+        else if (player.getHp() - damageToPlayer <= 0)
         {
-            document.getElementById("status_info").innerHTML = "Opponent Wins! Player Faints!";
-            playerHp = 0;
-            opponentHp -= damageToOpponent;
+            document.getElementById("game_status_info").innerHTML = "Opponent Wins! Player Faints!";
+            player.setHp(0);
+            opponent.setHp(opponent.getHp()-damageToOpponent);
             gameOver = true;
         }
-        else if (opponentHp - damageToOpponent <= 0)
+        else if (opponent.getHp() - damageToOpponent <= 0)
         {
-            document.getElementById("status_info").innerHTML = "Player Wins! Opponent Faints!";
-            playerHp -= damageToPlayer;
-            opponentHp = 0;
+            document.getElementById("game_status_info").innerHTML = "Player Wins! Opponent Faints!";
+            player.setHp(player.getHp()-damageToPlayer);
+            opponent.setHp(0);
             gameOver = true;
         }
         else
         {
-            playerHp -= damageToPlayer;
-            opponentHp -= damageToOpponent;
+            player.setHp(player.getHp()-damageToPlayer);
+            opponent.setHp(opponent.getHp()-damageToOpponent);
         }
-        document.getElementById("player_hp").innerHTML = playerHp;
-        document.getElementById("opponent_hp").innerHTML = opponentHp;
+        document.getElementById("player_hp").innerHTML = player.getHp();
+        document.getElementById("opponent_hp").innerHTML = opponent.getHp();
     }
 };
